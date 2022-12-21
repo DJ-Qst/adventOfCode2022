8202c88d8bf07b26b29565defb1068f4a9b61cb9
Jorden Quast <jordenquast@gmail.com>
2022-12-21 08:12:28 -0600

    Good news, part 1 is done. Bug fixed: If you had to do the same step multiple times, it could not handle a double digit number

--- adventOfCode/day9.cpp
+++ adventOfCode/day9.cpp
@@ -15,10 +15,10 @@ int day9::part1() {
 	std::pair <int, int> tailPos(0, 0);
 	std::vector<std::pair<int, int>> allTailPos;
 	int xDiff, yDiff;
-	// int breakPoint=0;  // This is for testing
+
 	for (std::string step : steps) {
 		// Stay on this step for as many times as required
-		for (int i = 1; i <= (int(step[2]) - 48); i++) {		
+		for (int i = 1; i <= (stoi(step.substr(1))); i++) {
 			// Update head position
 			if (step[0] == 'U') {
 				headPos.second += 1;
@@ -36,7 +36,7 @@ int day9::part1() {
 			// Check how far apart the head and tail is
 			xDiff = headPos.first - tailPos.first;
 			yDiff = headPos.second - tailPos.second;
-			
+
 			// Update tail x-coordinate
 			if (xDiff > 1) {
 				tailPos.first += 1;
@@ -52,7 +52,7 @@ int day9::part1() {
 			else if (yDiff < -1) {
 				tailPos.second -= 1;
 			}
-			
+
 			// Extra changes in case of diagonals
 			if ((abs(xDiff) > 1) && (yDiff == 1)) {
 				tailPos.second += 1;
@@ -67,21 +67,12 @@ int day9::part1() {
 			else if ((abs(yDiff) > 1) && (xDiff == -1)) {
 				tailPos.first -= 1;
 			}
-			
-			//std::cout << "Step: " << step << ", Head: " << headPos.first << " " << headPos.second;
-			//std::cout << ", Tail: " << tailPos.first << " " << tailPos.second << std::endl;
+
 			// Check if current tail position is unique
 			if (std::find(allTailPos.begin(), allTailPos.end(), tailPos) == allTailPos.end()) {
 				allTailPos.push_back(tailPos);
 			}
 		}
-		/*
-		if (breakPoint == 25) {
-			break;
-		}
-		breakPoint++;
-		*/
-		//std::cout << step << std::endl;
 	}
 
 	return allTailPos.size();
